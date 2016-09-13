9b939b333df029998ab82ee3c6622b2aa901a851
Prema Chand Alugu <premaca@gmail.com>
2016-09-14 04:20:58 +0530

    Bring on Tesla

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,6 +5,9 @@ TARGET_KERNEL_CONFIG := cyanogenmod_wt88047_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
+# Rom Tool Chain
+TARGET_GCC_VERSION_EXP := 4.9
+
 BOARD_KERNEL_CMDLINE += \
     sched_enable_hmp=1 \
     phy-msm-usb.floated_charger_enable=1
