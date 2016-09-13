26fd8973aed8349c728be08b2ed7ca33a6fb40f4
Prema Chand Alugu <premaca@gmail.com>
2016-09-14 00:27:31 +0530

    Remove IMS services
    Change-Id: I7da6e8ca4a5d74adea6141db0917f602a8bf8954

--- rootdir/etc/init.qti.ims.sh
+++ rootdir/etc/init.qti.ims.sh
@@ -28,29 +28,29 @@
 #
 #
 
-dir0=/data
-trigger_file=$dir0/ims_diabled
-ims_disabled=`getprop persist.ims.disabled`
-target=`getprop ro.build.product`
+#dir0=/data
+#trigger_file=$dir0/ims_diabled
+#ims_disabled=`getprop persist.ims.disabled`
+#target=`getprop ro.build.product`
 
 #if [ ! -e $trigger_file ]; then
 #   for future use in doing conditional debugging
 #else
 #
 #fi
-echo "$ims_disabled"
-echo "$target"
+#echo "$ims_disabled"
+#echo "$target"
 
-if [ "$ims_disabled" = "0" ]; then
-    echo "ims will be enabled"
-    setprop service.qti.ims.enabled 1
-    exit
-fi
+#if [ "$ims_disabled" = "0" ]; then
+#    echo "ims will be enabled"
+#    setprop service.qti.ims.enabled 1
+#    exit
+#fi
 
-if [ "$ims_disabled" = "1" ] || [ "$target" = "msm8909_512" ]; then
-    echo "ims is disabled"
-    setprop service.qti.ims.enabled 0
-else
-    echo "ims is enabled"
-    setprop service.qti.ims.enabled 1
-fi
+#if [ "$ims_disabled" = "1" ] || [ "$target" = "msm8909_512" ]; then
+#    echo "ims is disabled"
+#    setprop service.qti.ims.enabled 0
+#else
+#    echo "ims is enabled"
+#    setprop service.qti.ims.enabled 1
+#fi
